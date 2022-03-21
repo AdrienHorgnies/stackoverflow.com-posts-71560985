d59a317896ab45f390f405c7ae6a87c8228c0ecf
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2022-03-21 23:36:47 +0100

    try using mojo executor
    It works, but I can't access or mutate the mojo...

--- report-maven-plugin/src/main/java/org/example/plugins/MyMojo.java
+++ report-maven-plugin/src/main/java/org/example/plugins/MyMojo.java
@@ -1,9 +1,48 @@
 package org.example.plugins;
 
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.codehaus.mojo.license.AggregatorAddThirdPartyMojo;
+import org.apache.maven.project.MavenProject;
 
 @Mojo(name = "my-goal", aggregator = true, requiresDependencyResolution = ResolutionScope.TEST)
-public class MyMojo extends AggregatorAddThirdPartyMojo {
+public class MyMojo extends AbstractMojo {
+
+    @Component
+    private MavenProject mavenProject;
+
+    @Component
+    private MavenSession mavenSession;
+
+    @Component
+    private BuildPluginManager pluginManager;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        System.out.println("==========================================");
+        executeMojo(
+                plugin(
+                        groupId("org.codehaus.mojo"),
+                        artifactId("license-maven-plugin"),
+                        version("2.0.0")
+                ),
+                goal("aggregate-add-third-party"),
+                configuration(
+                        element(name("includeTransitiveDependencies"), "false")
+                ),
+                executionEnvironment(
+                        mavenProject,
+                        mavenSession,
+                        pluginManager
+                )
+        );
+        System.out.println("==========================================");
+    }
 }
